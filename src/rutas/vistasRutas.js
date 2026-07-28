fadeaf40f03fa8f1225f11effae68c3d7ea49dbf
Esmeralda1305 <2336000046@utna.edu.mx>
2026-07-28 12:34:50 -0600

    Agregar vistas de autenticación

--- src/rutas/vistasRutas.js
+++ src/rutas/vistasRutas.js
@@ -8,4 +8,18 @@ router.get('/', (req, res) => {
   });
 });
 
+// Vista de iniciar sesión
+router.get('/iniciar-sesion', (req, res) => {
+  res.render('paginas/iniciar-sesion', {
+    titulo: 'Iniciar Sesión'
+  });
+});
+
+// Vista de registro
+router.get('/registro', (req, res) => {
+  res.render('paginas/registro', {
+    titulo: 'Registro de Usuario'
+  });
+});
+
 module.exports = router;
