e8574b82cd7a7cac072a06457d7209308adc7e60
RAIDCO <alexmtz135626@gmail.com>
2026-07-28 17:33:27 -0600

    Se configuro el contenedor de docker, y se agrego la vista de subastas, adenas de los componentes como el navbar y sidebar

--- src/rutas/vistasRutas.js
+++ src/rutas/vistasRutas.js
@@ -1,10 +1,43 @@
 const express = require('express');
 const router = express.Router();
 
-// Ruta principal inicial (Página de inicio)
+// Ruta principal / Landing Page
 router.get('/', (req, res) => {
   res.render('paginas/inicio', {
-    titulo: 'Inicio - Subastas'
+    titulo: 'Inicio - SubastasPro'
+  });
+});
+
+// Catálogo de subastas activas
+router.get('/subastas', (req, res) => {
+  // TODO: Obtener subastas de la BD cuando los modelos estén listos
+  res.render('paginas/subastas', {
+    titulo: 'Subastas Activas - SubastasPro',
+    paginaActual: 'subastas',
+    subastas: [],
+    usuario: null
+  });
+});
+
+// Detalle de una subasta individual
+router.get('/subastas/:id', (req, res) => {
+  // TODO: Buscar subasta por ID en la BD
+  res.render('paginas/detalle-subasta', {
+    titulo: 'Detalle de Subasta - SubastasPro',
+    paginaActual: 'subastas',
+    subasta: null,
+    usuario: null
+  });
+});
+
+// Historial de subastas del usuario
+router.get('/historial', (req, res) => {
+  // TODO: Obtener historial de subastas del usuario autenticado
+  res.render('paginas/historial', {
+    titulo: 'Mi Historial - SubastasPro',
+    paginaActual: 'historial',
+    historial: [],
+    usuario: null
   });
 });
 
